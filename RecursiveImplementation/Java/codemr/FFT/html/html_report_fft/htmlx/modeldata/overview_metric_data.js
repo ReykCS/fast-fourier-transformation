4ca2a317c330f407b4ae681e007bfbdc062e5054
ReykCS <reyk-carstens@web.de>
2020-02-29 21:40:49 +0100

    removes codemr

--- RecursiveImplementation/Java/codemr/FFT/html/html_report_fft/htmlx/modeldata/overview_metric_data.js
+++
@@ -1,1 +0,0 @@
-var CodeMR_ProjectMetrics = {"Project Name":"FFT working set: {FFT}", "Number of Highly Problematic Classes":0, "Number of Entities":1, "Number of Problematic Classes":0, "Class Lines of Code":25, "Number of External Packages":4, "Number of Packages":1, "Number of External Entities":7}
